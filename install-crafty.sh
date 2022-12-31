345690646677b689859dab3d390edf7953d2b9b5
Google61 <golubev634@gmail.com>
2022-12-31 11:19:12 +0300

    Update install-crafty.sh

--- install-crafty.sh
+++ install-crafty.sh
@@ -1,10 +1,11 @@
-sudo apt install git python3 python3-dev python3-pip software-properties-common openjdk-8-jdk openjdk-8-jre
-sudo mkdir -p /var/opt/minecraft/crafty
-sudo mkdir -p /var/opt/minecraft/server
+# sudo install-crafty.sh !!!
+apt install git python3 python3-dev python3-pip software-properties-common openjdk-8-jdk openjdk-8-jre
+mkdir -p /var/opt/minecraft/crafty
+mkdir -p /var/opt/minecraft/server
 cd /var/opt/minecraft/crafty
-sudo git clone https://gitlab.com/crafty-controller/crafty-4.git
-sudo python3 -m venv venv
+git clone https://gitlab.com/crafty-controller/crafty-4.git
+python3 -m venv venv
 cd crafty-4
 pip3 install --no-cache-dir -r requirements.txt
-sudo mv $GITHUB_WORKSPACE/main.py ./main.py
-sudo python3 main.py
+mv $GITHUB_WORKSPACE/main.py ./main.py
+python3 main.py
