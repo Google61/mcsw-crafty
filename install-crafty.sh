ab9cda298e33cea4346ace4ed5c824986c3626c6
Google61 <golubev634@gmail.com>
2022-12-31 03:56:39 +0300

    Create install-crafty.sh

---
+++ install-crafty.sh
@@ -0,0 +1,9 @@
+sudo apt install git python3 python3-dev python3-pip software-properties-common openjdk-8-jdk openjdk-8-jre
+sudo mkdir -p /var/opt/minecraft/crafty
+sudo mkdir -p /var/opt/minecraft/server
+cd /var/opt/minecraft/crafty
+sudo git clone https://gitlab.com/crafty-controller/crafty-4.git
+sudo python3 -m venv venv
+cd crafty-4
+pip3 install --no-cache-dir -r requirements.txt
+python3 main.py
